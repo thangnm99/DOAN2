fd7dc0e37cc59fd9435edf2dd9220ba234f8fa15
unknown <thang.lhp1999@gmail.com>
2020-12-13 21:03:56 +0700

    edit UI

--- Application.py
+++ Application.py
@@ -5,52 +5,49 @@ import cv2
 from tkinter  import filedialog
 import main
 
-def Open_File():
-    global display_area,imgtk,kq,v
+def Handle_File():
+    global display_area,imgtk,text
     filedir = filedialog.askopenfilename()
     if filedir != '' :
-        print(filedir)
         img = cv2.imread(filedir)
         img = cv2.resize(img,(400,200))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         imgtk = ImageTk.PhotoImage(image = Image.fromarray(img))
         display_area.create_image(0,0,image=imgtk, anchor = NW)
         predict_result = main.recognize.recog(filedir)
-        v.set(predict_result)
+        text.set(predict_result)
 
 
 windows = Tk()
 windows.title("Number reader")
-windows.geometry('800x300')
+windows.geometry('800x200')
 windows.resizable(width=False, height= False)
 win_font = font.Font(size = 20)
-# -----------------------------------------
-menubar = Frame(windows)
-menubar.pack(fill = X)
 
-filebt = Button(menubar, text='File',relief  = 'flat', command = Open_File)
-filebt.pack(side=LEFT)
-# toolbt = Button(menubar, text='Tool',relief  = 'flat')
-# toolbt.pack(side=LEFT)
-separateline = Canvas(windows,width=810, height = 1, bg = 'gray')
-separateline.pack(side = TOP)
+#------------------------------------------
+filemenu = Menu(windows)
+m1 = Menu(filemenu,tearoff=0)
+m1.add_command(label = 'Open File',command =Handle_File)
+m1.add_command(label = 'Exit', command =quit)
+filemenu.add_cascade(label = 'File', menu = m1)
 #------------------------------------------
 display_area = Canvas(windows,width =400, height = 200, bg = 'white')
+display_area.grid(column = 0, row = 0)
 imgtk = None
-display_area.pack(side = LEFT)
+# display_area.pack(side = TOP)
 
 #------------------------------------------
-label = Label(windows, text='GIA TRI: ')
+label = Label(windows, text='Predict result: ')
 label['font'] =win_font
-label.pack(side = LEFT)
+label.grid(column = 1, row = 0)
 
 #------------------------------------------
 
-v= StringVar()
-v.set('')
-entry = Entry(windows,textvariable = v)
+text= StringVar()
+text.set('')
+entry = Entry(windows,textvariable = text)
 entry['font'] = win_font
-entry.pack(side = LEFT)
+entry.grid(column = 2, row = 0)
 
-
+windows.config(menu= filemenu)
 windows.mainloop()
